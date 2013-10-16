200336f8d6c0b85e57d4ec34945549700bae2c66
Can Ibanoglu <can@howyoucod.in>
2013-10-16 12:50:36 +0300

    Hard coded the templates dir into settings.py. Bad stuff. Chapter 4

--- rango/views.py
+++ rango/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Rango says "Hello World!"')
+
+def about(request):
+    return HttpResponse('Rango says: Here is the about page.')
