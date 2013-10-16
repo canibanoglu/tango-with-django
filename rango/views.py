d737dc37a84f1d0a2bbaeff1a219dbfdc37bb002
Can Ibanoglu <can@howyoucod.in>
2013-10-17 00:03:19 +0300

    Fixed the category view and the corresponding template so that the template correctly displays an error if the requested category is not present. Modified the index view and the corresponging template so that it displays the most viewed pages in descending order. Chose not to implement the URL encoder/decoder functions as they can't be implemented the way authors ask me to. Exercises for Chapter 6.

--- rango/views.py
+++ rango/views.py
@@ -14,6 +14,9 @@ def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
 
+    pages_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = pages_list
+
     # We loop through each category returned, and create a URL attribute.
     # This attribute stores an encoded URL (e.g. spaces replaced with underscores).
     for category in category_list:
@@ -42,6 +45,7 @@ def category(request, category_name_url):
         # If we can't, the .get() method raise a DoesNotExist exception.
         # So the .get() method return one model instance or raises an exception.
         category = Category.objects.get(name = category_name)
+        context_dict['category'] = category
 
         # Retrieve all of the associated pages.
         # Note that filter returns >= 1 model instance.
@@ -49,7 +53,7 @@ def category(request, category_name_url):
 
         # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
-    except:
+    except Category.DoesNotExist:
         # We get here if we didn't find the specified category.
         # Don't do anything - the template displays the "no category" message for us.
         pass
